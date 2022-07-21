06296af5a6572a1abcaa4fd4aea4707c1ee2a9d9
svphlek <sereil@live.ca>
2022-07-21 15:58:29 -0400

    Day 05 - Password Generator

---
+++ Day 05/PasswordGenerator.py
@@ -0,0 +1,45 @@
+#Password Generator Project
+
+#Day 5 of 100 Days of Code
+
+#Sereil-Vann Phlek - 2022-07-21
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+##############################################################
+
+#Using a list to use the random.shuffle function later
+password_list = []
+
+#If nr_letters = 4 then:
+# This loop below will loop 4 times, and char will be a number between 1 - 4
+for char in range(1,nr_letters + 1):    
+   password_list.append(random.choice(letters))
+
+for char in range(1,nr_symbols + 1):
+    password_list.append(random.choice(numbers))
+    
+for char in range(1,nr_numbers + 1):   
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your password could be: {password}")
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 100-days-of-code
+
+I will master the Python programming language by building 100 unique projects over 100 days.
+I will learn automation, game, app and web development, data science and machine learning all using Python.
+I will be able to program in Python professionally
+I will learn Selenium, Beautiful Soup, Request, Flask, Pandas, NumPy, Scikit Learn, Plotly, and Matplotlib.
