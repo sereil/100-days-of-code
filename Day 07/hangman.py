0feb10597c9195de35e8f7ef9c47c63ec80deb5c
svphlek <sereil@live.ca>
2022-07-24 11:45:34 -0400

    improve ux for Day 07

--- Day 07/hangman.py
+++ Day 07/hangman.py
@@ -20,7 +20,7 @@ lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 logo = hangman_art.logo
-game_over_art = hangman_art.logo
+game_over_art = hangman_art.game_over_art
 print(f"""{logo}
 Welcome to the Hangman game!
 """)
@@ -48,21 +48,22 @@ while not end_of_game:
         #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-            #This print is out of order and I kind of don't like it anyways since the list shows if they got it right or not.
-            # print(f"{guess} is correct!")
+            
             
 
     #Check if user is wrong.
     print(f"Previous guesses: {wrong_guesses}")  
     if guess not in chosen_word:
         #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.               
-        print(f"{guess} is incorrect! You lose a life.")
+        print(f"You guessed '{guess}' it is incorrect! You lose a life.")
         wrong_guesses.append(guess)
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print(f"""You lose.
-            {game_over_art}""")
+            print(f"""You lose.""")
+            
+    else:
+        print(f"You guessed '{guess}', it is correct!")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -75,3 +76,5 @@ while not end_of_game:
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     stages = hangman_art.stages
     print(stages[lives])
+    if lives == 0:
+        print(game_over_art)
