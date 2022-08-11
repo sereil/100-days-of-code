fb4c5b6bc663045e3cb09cdcb857e338288f464f
sphlek <sereil@live.ca>
2022-08-10 23:30:57 -0400

    Day 17 - Quiz

---
+++ Day 17/data.py
@@ -0,0 +1,18 @@
+# question_data = [
+#     {"category": "Vehicles", "type": "multiple", "difficulty": "easy", "text": "Where are the cars of the brand &quot;Ferrari&quot; manufactured?", "answer": "Italy", "inanswers": ["Romania", "Germany", "Russia"]}, {"category": "Entertainment: Music", "type": "boolean", "difficulty": "easy", "text": "The music video to The Buggle&#039;s &quot;Video Killed the Radio Star&quot; was the first music video to broadcast on MTV.", "answer": "True", "inanswers": ["False"]}, {"category": "Entertainment: Film", "type": "multiple", "difficulty": "medium", "text": "Which movie sequel had improved box office results compared to its original film?", "answer": "Toy Story 2", "inanswers": ["Sin City: A Dame to Kill For", "Speed 2: Cruise Control", "Son of the Mask"]}, {"category": "Geography", "type": "boolean", "difficulty": "easy", "text": "There are no deserts in Europe.", "answer": "True", "inanswers": ["False"]}, {"category": "Science & Nature", "type": "multiple", "difficulty": "easy", "text": "The asteroid belt is located between which two planets?", "answer": "Mars and Jupiter", "inanswers": ["Jupiter and Saturn", "Mercury and Venus", "Earth and Mars"]}, {
+#         "category": "Geography", "type": "multiple", "difficulty": "medium", "text": "What is the smallest country in South America by area?", "answer": "Suriname", "inanswers": ["Brazil", "Uruguay", "Chile"]}, {"category": "Entertainment: Video Games", "type": "multiple", "difficulty": "hard", "text": "What programming language was used to create the game &quot;Minecraft&quot;?", "answer": "Java", "inanswers": ["HTML 5", "C++", "Python"]}, {"category": "Entertainment: Music", "type": "multiple", "difficulty": "medium", "text": "What is rapper Drake&#039;s real name?", "answer": "Aubrey Graham", "inanswers": ["Shaun Carter", "Dwayne Carter", "Andre Young"]}, {"category": "Entertainment: Video Games", "type": "multiple", "difficulty": "medium", "text": "Which of these is NOT a name of a city in the main island of PLAYERUNKNOWN&#039;S BATTLEGROUNDS?", "answer": "Belushya Guba", "inanswers": ["Yasnaya Polyana", "Pochinki", "Georgopol"]}, {"category": "Art", "type": "multiple", "difficulty": "easy", "text": "Which painting was not made by Vincent Van Gogh?", "answer": "The Ninth Wave", "inanswers": ["Caf&eacute; Terrace at Night", "Bedroom In Arles", "Starry Night"]}]
+
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
