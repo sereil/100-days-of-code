9971ca8c204e96d559eede480ae202113f289756
svphlek <sereil@live.ca>
2022-10-03 11:03:14 -0400

    Day 31 - Flash Card

--- Day 31/main.py
+++ Day 31/main.py
@@ -6,42 +6,44 @@ FONT_LANG = ("Arial", 40, "italic")
 FONT_WORD = ("Arial", 40, "bold")
 
 BACKGROUND_COLOR = "#B1DDC6"
-current_card_id = None
+current_card = {}
+to_learn = {}
 
-
-def knows_word():
-    global current_card_id
-    new_dict = data_dict["English"].pop(current_card_id)
-    new_dict = data_dict["French"].pop(current_card_id)
-    dict_df = pandas.DataFrame(new_dict)
-    dict_df.to_csv("..\\100-days-of-code\Day 31\data\\words_to_learn.csv")
+def known_word():
+    global current_card
+    data_dict.remove(current_card)
+    dict_df = pandas.DataFrame(data_dict)
+    dict_df.to_csv("..\\100-days-of-code\Day 31\data\\words_to_learn.csv", index=False)
     new_card()
     
 
 def new_card():
-    global current_card_id
-    current_card_id = random.randint(0,100)
-    card_french = (data_dict['French'].get(current_card_id)).title()
-    canvas_card.itemconfig(card_word, text=f"{card_french}")
+    global current_card, flip_timer    
+    window.after_cancel(flip_timer)
+    if(len(data_dict) == 0):
+        print("No more cards to learn")
+        exit
+    current_card = random.choice(data_dict)
+    word_french = current_card["French"].title()
+    canvas_card.itemconfig(card_word, text=f"{word_french}")
     canvas_card.itemconfig(canvas_img, image=card_front_img)
-    window.after(3000,func=change_to_english)
+    flip_timer = window.after(3000,func=change_to_english)
 
 def change_to_english():
-    global current_card_id
-    card_english = (data_dict['English'].get(current_card_id)).title()
-    canvas_card.itemconfig(card_word, text=f"{card_english}")
+    global current_card
+    canvas_card.itemconfig(card_word, text=f"{current_card['English'].title()}")
     canvas_card.itemconfig(canvas_img, image=card_back_img)
 
 window = Tk()
 window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR,padx=50, pady=50, height=626, width=900)
-
-
-Data = pandas.read_csv("..\\100-days-of-code\Day 31\data\\french_words.csv")
-
-data_dict = pandas.DataFrame(Data).to_dict()
-
-print(data_dict)
+flip_timer = window.after(3000,func=change_to_english)
+try:
+    data = pandas.read_csv("..\\100-days-of-code\Day 31\data\\words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("..\\100-days-of-code\Day 31\data\\french_words.csv")
+finally:
+    data_dict = pandas.DataFrame(data).to_dict(orient="records") #Records prints it out as a list
 
 
 card_front_img  = PhotoImage(file="..\\100-days-of-code\Day 31\images\card_front.png")
@@ -57,7 +59,7 @@ card_lang = canvas_card.create_text(400,150, text="Language", font=FONT_LANG)
 card_word =canvas_card.create_text(400,263, font=FONT_WORD)
 new_card()
 
-checkmark_btn = Button(image=checkmark_img, border=0, highlightthickness=0,command=knows_word)
+checkmark_btn = Button(image=checkmark_img, border=0, highlightthickness=0,command=known_word)
 checkmark_btn.grid(row=1,column=1)
 cross_btn = Button(image=cross_img, border=0, highlightthickness=0,command=new_card)
 cross_btn.grid(row=1,column=0)
